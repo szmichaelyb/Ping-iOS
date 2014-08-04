96242b3458a1d6534c6566452b7014621c8676bd
Rishabh Tayal <rtayal11@gmail.com>
2014-08-04 17:23:49 -0500

    commit.

--- Code/Android/src/com/rtayal/gocandid/GoCandid.java
+++ Code/Android/src/com/rtayal/gocandid/GoCandid.java
@@ -1,9 +1,12 @@
 package com.rtayal.gocandid;
 
 import android.app.Application;
+import android.util.Log;
+
 import com.parse.Parse;
-import com.parse.ParseAnalytics;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class GoCandid extends Application {
 
@@ -16,10 +19,6 @@ public class GoCandid extends Application {
 		// TODO Auto-generated method stub
 		super.onCreate();
 		Parse.initialize(this, "RjjejatHY8BsqER68vg48jtr9nRv0FVAfKqryjja", "hTwjS9Ng9azIQoOfpQ6xeYX3Ah8mesiCWGt0gz3b");
-		
-		ParseObject testObject = new ParseObject("TestObject");
-		testObject.put("foo", "bar");
-		testObject.saveInBackground();
 	}
 
 }
